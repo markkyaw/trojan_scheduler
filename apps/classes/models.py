b73bac3e7ff715f62ae459bfee0c0672f8ed7683
Jamie Leodones <leodones@usc.edu>
2022-01-27 22:29:44 -0800

    Committing changes to models.py for BasicClassSession and Instructor models + aux models.

--- apps/classes/models.py
+++ apps/classes/models.py
@@ -1,3 +1,5 @@
+from curses.ascii import NUL
+
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -9,7 +11,20 @@ class ClassSemester(models.IntegerChoices):
     FALL = 3, _("Fall")
 
 
+class SectionType(models.IntegerChoices):
+    LECTURE = 1, _("Lecture")
+    DISCUSSION = 2, _("Discussion")
+    LAB = 3, _("Lab")
+    QUIZ = 4, _("Quiz")
+
+
 # Create your models here.
+class Instructor(models.Model):
+    name = models.CharField(
+        max_length=200,
+    )
+
+
 class BasicClassInfo(models.Model):
     """
     class_code: CSCI
@@ -38,3 +53,43 @@ class BasicClassInfo(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(12)],
     )
     description = models.TextField()
+
+
+class BasicClassSection(BasicClassInfo):
+    """
+    The model will need to store
+    - Section - from parent
+    - Session - done
+    - Type - done
+    - Time
+        - Start time
+        - End time
+    - Days - done
+    - Instructor - done
+    - Location
+        - Building
+        - Room #
+
+    Notes:
+    - Maybe implement default values for future semesters when not all values have been assigned.
+    """
+
+    session = models.CharField(
+        max_length=3,
+    )
+    type = models.IntegerField(choices=SectionType.choices)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    days = models.CharField(
+        max_length=7,
+        default="0000000",
+    )
+    instructor = models.ManyToManyField(Instructor)
+    location_building = models.CharField(
+        max_length=50,
+    )
+    location_room = models.CharField(
+        max_length=50,
+        default=None,
+        null=True,
+    )
