9924a9616056a8a31178eed71d12d120bda98b6f
Emma Leihe <leihe@usc.edu>
2022-02-18 22:06:40 -0800

    fixed errors in scraping day, time; modified models to account for lecture-labs

--- scraper_classes.py
+++ scraper_classes.py
@@ -3,7 +3,7 @@ from datetime import time
 import requests
 from bs4 import BeautifulSoup
 
-from apps.classes.models import BasicClassInfo, BasicClassSection, Instructor
+# from apps.classes.models import BasicClassInfo, BasicClassSection, Instructor
 
 
 class Parser:
@@ -51,64 +51,68 @@ class Parser:
             descriptn = cl.find(class_="catalogue").get_text().strip()
 
             # Create database object
-            c = BasicClassInfo.objects.create(
-                class_code=cl_code,
-                class_number=cl_num,
-                year=year,
-                semester=sem,
-                min_units=min_unit,
-                max_units=max_unit,
-                description=descriptn,
-            )
+            # c = BasicClassInfo.objects.create(
+            #     class_code=cl_code,
+            #     class_number=cl_num,
+            #     year=year,
+            #     semester=sem,
+            #     min_units=min_unit,
+            #     max_units=max_unit,
+            #     description=descriptn,
+            # )
 
             # Parse Sections
-            self.parseSections(cl, c)
+            # for when django import works
+            # self.__parseSections(cl, c)
+            self.__parseSections(cl)
 
         return None
 
-    # REMOVE CLASS_ID AFTER TESTING
-    def parseSections(self, class_info: BeautifulSoup, c: BasicClassInfo, class_id):
-        # class_info = self.soup.find("div", id=class_code + "-" + class_id)
+    # for when django import works
+    # def parseSections(self, class_info: BeautifulSoup, c: BasicClassInfo):
+    def __parseSections(self, class_info: BeautifulSoup):
         s_table = class_info.select_one("table[class~=sections]")
-
-        # FOR TESTING
-        id_list = []
-        ssn_list = []
-        type_list = []
-        st_times = []
-        end_times = []
-        days_list = []
-        instr_list = []
-        loc_bldgs = []
-        loc_rooms = []
 
         # REMOVE ALL APPENDS AFTER TESTING
         for s in s_table.select("tr"):  # Get all the table rows
             if "headers" in s["class"]:  # Skip header row
                 continue
+            if s.select_one(".section") == None:
+                continue
+
             # Section ID
-            id = s.select_one(".section").get_text().strip()
-            id_list.append(id)
+            scraped_id = s.select_one(".section")
+            id = ""
+            if scraped_id != None:
+                id = scraped_id.get_text().strip()
 
             # Session
-            ssn = s.select_one(".session > a").get_text().strip()
-            ssn_list.append(ssn)
+            scraped_ssn = s.select_one(".session > a")
+            ssn = ""
+            if scraped_ssn != None:
+                ssn = scraped_ssn.get_text().strip()
 
             # Session type : 1=lect, 2=disc, 3=lab, 4=quiz
-            type_name = s.select_one(".type").get_text().strip()
+            scraped_type = s.select_one(".type")
             type_int = None
-            if type_name == "Lecture":
-                type_int = 1
-            elif type_name == "Discussion":
-                type_int = 2
-            elif type_name == "Lab":
-                type_int = 3
-            elif type_name == "Quiz":
-                type_int = 4
-            type_list.append(type_int)
+            if scraped_type != None:
+                type_name = scraped_type.get_text().strip()
+                if type_name == "Lecture":
+                    type_int = 1
+                elif type_name == "Discussion":
+                    type_int = 2
+                elif type_name == "Lab":
+                    type_int = 3
+                elif type_name == "Quiz":
+                    type_int = 4
+                elif type_name == "Lecture-Lab":
+                    type_int = 5
 
             # Time
-            timeTxt = s.select_one(".time").get_text().strip()
+            scraped_time = s.select_one(".time")
+            timeTxt = ""
+            if scraped_time != None:
+                timeTxt = scraped_time.get_text().strip()
             startTime: time = None
             endTime: time = None
             hyphen = timeTxt.find("-")
@@ -126,101 +130,66 @@ class Parser:
                 endMin = int(end[endColon + 1 :])
 
                 if am_pm == "pm":
-                    stHr += 12
-                    endHr += 12
+                    # for classes that start in am, end in pm (after 12:59)
+                    if endHr < stHr:
+                        endHr += 12
+                    # catches classes with start, end times around ~12 pm
+                    elif stHr < 12 and endHr < 12:
+                        stHr += 12
+                        endHr += 12
 
                 # convert to datetime obj format
                 startTime = time(stHr, stMin)
                 endTime = time(endHr, endMin)
 
-            st_times.append(startTime)
-            end_times.append(endTime)
-
             # Day(s)
+            days_temp = list("0000000")
+            # In case of TBA, defaults to model default "0000000"
+            days_txt = ""
+            scraped_days = s.select_one(".days")
+            if scraped_days != None:
+                days_txt = scraped_days.get_text().strip()
+            days = ["M", "Tu", "W", "Th", "F", "Sa", "Su"]
+            for i, day in enumerate(days):
+                idx = days_txt.find(day)
+                if idx != -1:
+                    days_temp[i] = "1"
             days_str = ""
-            days_txt = s.select_one(".days").get_text().strip()
-            if days_txt == "TBA":
-                days_str = "0000000"
-            else:
-                days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-                day_tracker = dict.fromkeys(days, "0")
-                i = 0
-                print(days_txt)
-                while days_txt.find(",") != -1:
-                    i += 2  # to account for the ', ' btwn days
-                    septr = days_txt.find(",", i)
-                    # get first 3 letters
-                    temp_day = days_txt[i:septr][i : i + 3]
-                    if temp_day in day_tracker:
-                        print("in while loop: " + temp_day)
-                        day_tracker[temp_day] = "1"
-                    i = septr
-                if days_txt[i : i + 3] in day_tracker:
-                    print("outside while loop: " + days_txt[i : i + 3])
-                    day_tracker[days_txt[i : i + 3]] = "1"
-                # Append all to string
-                for j in day_tracker:
-                    days_str += day_tracker[j]
-            days_list.append(days_str)
+            for n in days_temp:
+                days_str += n
 
             # Instructor(s)
-            instr_name = s.select_one(".instructor > a").get_text().strip()
-            instr = Instructor.objects.create(name=instr_name)
-            instr_list.append(instr_name)
+            scraped_instr = s.select_one(".instructor")
+            instr_name = ""
+            if scraped_instr != None:
+                instr_name = scraped_instr.get_text().strip()
 
             # Location
-            location = s.select_one(".location").get_text().strip()
+            scraped_loc = s.select_one(".location")
+            location = ""
+            if scraped_loc != None:
+                location = scraped_loc.get_text().strip()
             loc_bldg = location
             loc_room = location
             for i, char in enumerate(location):
                 if char.isdigit():
                     loc_bldg = location[:i]
-                    loc_bldgs.append(loc_bldg)
                     loc_room = location[i:]
-                    loc_rooms.append(loc_room)
                     break
-            # REMOVE ELSE AFTER TESTING
-            else:
-                # assume TBA or otherwise not formatted
-                loc_bldgs.append(location)
-                loc_rooms.append(location)
 
             # Create database object
-            sect: BasicClassSection = BasicClassSection.objects.create(
-                c,
-                section=id,
-                session=ssn,
-                section_type=type_int,
-                start_time=startTime,
-                end_time=endTime,
-                days=days_str,
-                location_building=loc_bldg,
-                location_room=loc_room,
-            )
-            sect.instructor.add(instr)
-
-        print("CSCI" + class_id)
-        print("Section ID: ")
-        print(id_list)
-        print("Session: ")
-        print(ssn_list)
-        print("Type: ")
-        print(type_list)
-        print("Start: ")
-        print(st_times)
-        print("End: ")
-        print(end_times)
-        print("Days: ")
-        print(days_list)
-        print("Instructor: ")
-        print(instr_list)
-        print("Location: ")
-        print(loc_bldgs)
-        print(loc_rooms)
+            # sect: BasicClassSection = BasicClassSection.objects.create(
+            #     c,
+            #     section=id,
+            #     session=ssn,
+            #     section_type=type_int,
+            #     start_time=startTime,
+            #     end_time=endTime,
+            #     days=days_str,
+            #     location_building=loc_bldg,
+            #     location_room=loc_room,
+            # )
+            # instr = Instructor.objects.create(name=instr_name)
+            # sect.instructor.add(instr)
 
         return None
-
-
-# Testing
-c1 = Parser("https://classes.usc.edu/term-20221/classes/csci/")
-c1.parseClasses()
