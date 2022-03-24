dfba76961cc634e3ebc0f88c41a2b5fc68661c1b
aliceyzhou <aliceyzh@usc.edu>
2022-03-23 21:09:05 -0700

    commit

--- trojanscheduler/urls.py
+++ trojanscheduler/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path, re_path
 
+from apps.classes.models import BasicClassInfo
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     re_path(r"^api/", include("api.urls"), name="api-urls"),
+    path("", BasicClassInfo.class_section_search(), name="section-search"),
+    path("", BasicClassInfo.class_code_search(), name="code-search"),
 ]
--- trojanscheduler/views.py
+++ trojanscheduler/views.py
@@ -1,25 +1,39 @@
-# I think these work in principle, but I don't know how to loop through all the possible objects
-# this only works if you call it on a specific object
+# stolen from github
+import json
 
-from models import BasicClassInfo, BasicClassSection
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from api.v1.classes.serializers import *
+from apps.classes.models import BasicClassInfo
 
 
-class BasicClassInfoView:
+class BasicClassInfoView(GenericAPIView):
+    serializer_class = BasicClassInfoSerializer
+
+    def get(self, request, format=None):
+        return_status = status.HTTP_200_OK
+        ret = BasicClassInfoSerializer(BasicClassInfo.objects.get(id=1)).data
+
+        return Response(ret, return_status)
+
     # Create a view that will return a list of basicclasssection objects based on class_code and class_section
     # Ex: If the query passes in "CSCI 102" expect to see all sections that contains CSCI 102
     #    If the query passes in "all" return all classes in basicclassinfo objects ordered alphabetically
 
-    def class_section_search(object, query):
+    def class_section_search(query):
         if query == "all":
-            return object
+            return BasicClassInfo.objects.order_by("class_code")
+
         code = query[: query.index(" ")]
         section = query[query.index(" ") + 1 :]
-        if object.class_code == code and object.class_section == section:
-            return object
+
+        return BasicClassInfo.objects.filter(class_code=code, class_section=section)
 
     # Create another view that returns a list of basicclassinfo objects based on class code
     # If the query passes in is "CSCI" expect to see all basicclassinfo that contains "CSCI" so it'll have CSCI 102, CSCI 103, ...
 
-    def class_code_search(object, code):
-        if object.class_code.equals(code):
-            return object.class_code + " " + object.class_section
+    def class_code_search(code):
+        return BasicClassInfo.objects.filter(class_code=code)
